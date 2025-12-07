60e11219829bb78960c92686b06931876a5f914e
unknown <sonofsubhashini@gmail.com>
2025-12-07 06:45:40 +0530

    feat: update Hero tsx file from available to hire to Open to work

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
             </span>
-            <span className="text-emerald-700 text-xs font-bold tracking-widest uppercase">Available for Hire</span>
+            <span className="text-emerald-700 text-xs font-bold tracking-widest uppercase">Open to work</span>
           </div>
 
           {/* Avatar with Ring */}
